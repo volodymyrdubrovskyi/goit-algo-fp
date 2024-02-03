cc5c4047b7946e1a845337e7c773df30b703d39d
volodymyrdubrovskyi <vladimir.dubrovskiy@gmail.com>
2024-02-03 18:46:39 +0200

    version 03.02.2024.18.46

--- task1.py
+++ task1.py
@@ -73,25 +73,88 @@ class LinkedList:
         current_node.next = temp    
         self.head = current_node
         return self
+    
+    # Сортування на базі алгоритму сортування вставками
+    def sort_list(self):
+        if self.head.next == None:
+            return self
+        sorted_list = LinkedList() # Створюємо окремий список и вважаємо його відсортированим
+        end_of_list = False
+        current_node = self.head.next
+        sorted_list.insert_at_beginning(self.head.data)
+        # Беремо кожний вузел не відсортованого масиву й вставляємо його у відповідне місце сортованого списку
+        while not end_of_list:
+            # якщо вузел не відсортованого масиву менше першого вузла відсортованого списку, то додаємо його вперед
+            if current_node.data <= sorted_list.head.data:  
+                sorted_list.insert_at_beginning(current_node.data)
+            # інакше шукаємо місце у відсортованому списку
+            else:
+                sorted_current_node = sorted_list.head
+                prev_node = sorted_current_node
+                while current_node.data > sorted_current_node.data:
+                    prev_node = sorted_current_node
+                    if sorted_current_node.next:
+                        sorted_current_node = sorted_current_node.next
+                    else:
+                        break
+                sorted_list.insert_after(prev_node, current_node.data)
+            # і так доки не пройдемо весь не відсортоаний список
+            if not current_node.next:
+                end_of_list = True
+            else:
+                current_node = current_node.next
+        return sorted_list
+    
+
+def combine_llists(llist1: LinkedList, llist2: LinkedList,):
+    # переходемо на востанній вузол першого списку
+    curr_node = llist1.head
+    while curr_node.next: 
+        curr_node = curr_node.next
+    # об'єднуємо списки:
+    curr_node.next = llist2.head
+    return llist1.sort_list()
+
+    
 
 def main():
     llist = LinkedList()
 
     # Вставляємо вузли в початок
-    llist.insert_at_beginning(5)
+    llist.insert_at_beginning(13)
     llist.insert_at_beginning(4)
+    llist.insert_at_beginning(7)
     llist.insert_at_beginning(3)
-    llist.insert_at_beginning(2)
-    llist.insert_at_beginning(1)
+    llist.insert_at_beginning(12)
+
+    #llist.delete_node(7)
     
     # Друк зв'язного списку
     print("Зв'язний список:")
     llist.print_list()
 
-
     print("Оберненний зв'язний список:")
     llist = llist.reverse_list()
     llist.print_list()
+
+    print("Відсортований зв'язний список:")
+    llist = llist.sort_list()
+    llist.print_list()
+
+
+    llist1 = LinkedList()
+    llist1.insert_at_beginning(21)
+    llist1.insert_at_beginning(19)
+    llist1.insert_at_beginning(17)
+    llist1.insert_at_beginning(16)
+    llist1.insert_at_beginning(10)
+
+    print("Зв'язний список 2:")
+    llist1.print_list()
+
+    llist1 = combine_llists(llist, llist1)
+    print("Об'єднаний список:")
+    llist1.print_list()
 
 if __name__ == '__main__':
     main()
